59cfeff4ccaccd30ddf52246d2040e9bc2e1a454
eimmermann <eimmermann@gmail.com>
2026-01-22 14:28:13 -0500

    Initial Airkid files - levels 1-24 done

---
+++ puzzles.js
@@ -0,0 +1,1262 @@
+window.PUZZLE_DATA = [
+  {
+    "id": 1,
+    "type": "square",
+    "pieces": {
+      "green": 1,
+      "yellow": 0,
+      "orange": 0
+    },
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "E"
+    },
+    "end": {
+      "col": 2,
+      "row": 0
+    }
+  },
+  {
+    "id": 2,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "S"
+    },
+    "end": {
+      "col": 2,
+      "row": 1
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 0
+    }
+  },
+  {
+    "id": 3,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 1,
+      "dir": "E"
+    },
+    "end": {
+      "col": 3,
+      "row": 2
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 0,
+      "orange": 1
+    }
+  },
+  {
+    "id": 4,
+    "type": "square",
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 1
+    },
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "E"
+    },
+    "end": {
+      "col": 3,
+      "row": 3
+    }
+  },
+  {
+    "id": 5,
+    "type": "square",
+    "end": {
+      "col": 3,
+      "row": 2
+    },
+    "pieces": {
+      "green": 0,
+      "yellow": 2,
+      "orange": 1
+    },
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "E"
+    }
+  },
+  {
+    "id": 6,
+    "type": "square",
+    "pieces": {
+      "green": 2,
+      "yellow": 0,
+      "orange": 2
+    },
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "S"
+    },
+    "end": {
+      "col": 3,
+      "row": 3
+    }
+  },
+  {
+    "id": 7,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "E"
+    },
+    "pieces": {
+      "green": 0,
+      "yellow": 0,
+      "orange": 3
+    },
+    "end": {
+      "col": 3,
+      "row": 3
+    }
+  },
+  {
+    "id": 8,
+    "type": "square",
+    "pieces": {
+      "green": 1,
+      "yellow": 0,
+      "orange": 1
+    },
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 3,
+      "row": 2
+    }
+  },
+  {
+    "id": 9,
+    "type": "square",
+    "pieces": {
+      "green": 0,
+      "yellow": 0,
+      "orange": 3
+    },
+    "end": {
+      "col": 3,
+      "row": 3
+    },
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "E"
+    }
+  },
+  {
+    "id": 10,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "E"
+    },
+    "pieces": {
+      "green": 0,
+      "yellow": 0,
+      "orange": 4
+    },
+    "end": {
+      "col": 0,
+      "row": 3
+    }
+  },
+  {
+    "id": 11,
+    "type": "square",
+    "pieces": {
+      "green": 0,
+      "yellow": 0,
+      "orange": 1
+    },
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 2,
+      "row": 1
+    }
+  },
+  {
+    "id": 12,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "SE"
+    },
+    "pieces": {
+      "green": 0,
+      "yellow": 1,
+      "orange": 1
+    },
+    "end": {
+      "col": 0,
+      "row": 3
+    }
+  },
+  {
+    "id": 13,
+    "type": "square",
+    "end": {
+      "col": 3,
+      "row": 2
+    },
+    "pieces": {
+      "green": 0,
+      "yellow": 1,
+      "orange": 1
+    },
+    "start": {
+      "col": 0,
+      "row": 2,
+      "dir": "E"
+    }
+  },
+  {
+    "id": 14,
+    "type": "square",
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 1
+    },
+    "start": {
+      "col": 0,
+      "row": 3,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 0,
+      "row": 0
+    }
+  },
+  {
+    "id": 15,
+    "type": "square",
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 1
+    },
+    "end": {
+      "col": 2,
+      "row": 1
+    },
+    "start": {
+      "col": 0,
+      "row": 3,
+      "dir": "E"
+    }
+  },
+  {
+    "id": 16,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "S"
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 0,
+      "orange": 1
+    },
+    "end": {
+      "col": 2,
+      "row": 3
+    }
+  },
+  {
+    "id": 17,
+    "type": "square",
+    "end": {
+      "col": 2,
+      "row": 0
+    },
+    "pieces": {
+      "green": 0,
+      "yellow": 1,
+      "orange": 2
+    },
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "S"
+    }
+  },
+  {
+    "id": 18,
+    "type": "square",
+    "pieces": {
+      "green": 0,
+      "yellow": 2,
+      "orange": 0
+    },
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 3,
+      "row": 1
+    }
+  },
+  {
+    "id": 19,
+    "type": "square",
+    "pieces": {
+      "green": 0,
+      "yellow": 6,
+      "orange": 0
+    },
+    "end": {
+      "col": 3,
+      "row": 3
+    },
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "SE"
+    }
+  },
+  {
+    "id": 20,
+    "type": "square",
+    "pieces": {
+      "green": 0,
+      "yellow": 6,
+      "orange": 0
+    },
+    "start": {
+      "col": 2,
+      "row": 0,
+      "dir": "W"
+    },
+    "end": {
+      "col": 2,
+      "row": 3
+    }
+  },
+  {
+    "id": 21,
+    "type": "square",
+    "pieces": {
+      "green": 0,
+      "yellow": 1,
+      "orange": 0
+    },
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "E"
+    },
+    "end": {
+      "col": 1,
+      "row": 1
+    }
+  },
+  {
+    "id": 22,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "E"
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 0,
+      "orange": 1
+    },
+    "end": {
+      "col": 3,
+      "row": 2
+    }
+  },
+  {
+    "id": 23,
+    "type": "square",
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 0
+    },
+    "end": {
+      "col": 3,
+      "row": 2
+    },
+    "start": {
+      "col": 1,
+      "row": 1,
+      "dir": "E"
+    }
+  },
+  {
+    "id": 24,
+    "type": "square",
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 1
+    },
+    "start": {
+      "col": 0,
+      "row": 3,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 3,
+      "row": 1
+    }
+  },
+  {
+    "id": 25,
+    "type": "square",
+    "start": {
+      "col": 1,
+      "row": 2,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 2,
+      "row": 2
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 0
+    }
+  },
+  {
+    "id": 26,
+    "type": "square",
+    "start": {
+      "col": 2,
+      "row": 2,
+      "dir": "NW"
+    },
+    "end": {
+      "col": 1,
+      "row": 2
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 0
+    }
+  },
+  {
+    "id": 27,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 3,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 3,
+      "row": 0
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 2,
+      "orange": 0
+    }
+  },
+  {
+    "id": 28,
+    "type": "square",
+    "start": {
+      "col": 3,
+      "row": 3,
+      "dir": "NW"
+    },
+    "end": {
+      "col": 0,
+      "row": 0
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 2,
+      "orange": 0
+    }
+  },
+  {
+    "id": 29,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 3,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 3,
+      "row": 0
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 2,
+      "orange": 0
+    }
+  },
+  {
+    "id": 30,
+    "type": "square",
+    "start": {
+      "col": 3,
+      "row": 3,
+      "dir": "NW"
+    },
+    "end": {
+      "col": 0,
+      "row": 0
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 2,
+      "orange": 0
+    }
+  },
+  {
+    "id": 31,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 3,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 3,
+      "row": 0
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 2,
+      "orange": 0
+    }
+  },
+  {
+    "id": 32,
+    "type": "square",
+    "start": {
+      "col": 3,
+      "row": 3,
+      "dir": "NW"
+    },
+    "end": {
+      "col": 0,
+      "row": 0
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 2,
+      "orange": 0
+    }
+  },
+  {
+    "id": 33,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 2,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 3,
+      "row": 1
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 2,
+      "orange": 0
+    }
+  },
+  {
+    "id": 34,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 1,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 3,
+      "row": 2
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 2,
+      "orange": 0
+    }
+  },
+  {
+    "id": 35,
+    "type": "square",
+    "start": {
+      "col": 1,
+      "row": 0,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 2,
+      "row": 3
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 1
+    }
+  },
+  {
+    "id": 36,
+    "type": "square",
+    "start": {
+      "col": 2,
+      "row": 3,
+      "dir": "NW"
+    },
+    "end": {
+      "col": 1,
+      "row": 0
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 1
+    }
+  },
+  {
+    "id": 37,
+    "type": "square",
+    "start": {
+      "col": 1,
+      "row": 0,
+      "dir": "SW"
+    },
+    "end": {
+      "col": 2,
+      "row": 3
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 1
+    }
+  },
+  {
+    "id": 38,
+    "type": "square",
+    "start": {
+      "col": 2,
+      "row": 3,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 1,
+      "row": 0
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 1
+    }
+  },
+  {
+    "id": 39,
+    "type": "square",
+    "start": {
+      "col": 3,
+      "row": 2,
+      "dir": "NW"
+    },
+    "end": {
+      "col": 0,
+      "row": 1
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 1
+    }
+  },
+  {
+    "id": 40,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 1,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 3,
+      "row": 2
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 1
+    }
+  },
+  {
+    "id": 41,
+    "type": "square",
+    "start": {
+      "col": 3,
+      "row": 2,
+      "dir": "SW"
+    },
+    "end": {
+      "col": 0,
+      "row": 1
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 1
+    }
+  },
+  {
+    "id": 42,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 1,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 3,
+      "row": 2
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 1
+    }
+  },
+  {
+    "id": 43,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 3,
+      "row": 3
+    },
+    "pieces": {
+      "green": 2,
+      "yellow": 1,
+      "orange": 0
+    }
+  },
+  {
+    "id": 44,
+    "type": "square",
+    "start": {
+      "col": 3,
+      "row": 0,
+      "dir": "SW"
+    },
+    "end": {
+      "col": 0,
+      "row": 3
+    },
+    "pieces": {
+      "green": 2,
+      "yellow": 1,
+      "orange": 0
+    }
+  },
+  {
+    "id": 45,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 3,
+      "row": 3
+    },
+    "pieces": {
+      "green": 2,
+      "yellow": 1,
+      "orange": 0
+    }
+  },
+  {
+    "id": 46,
+    "type": "square",
+    "start": {
+      "col": 3,
+      "row": 0,
+      "dir": "SW"
+    },
+    "end": {
+      "col": 0,
+      "row": 3
+    },
+    "pieces": {
+      "green": 2,
+      "yellow": 1,
+      "orange": 0
+    }
+  },
+  {
+    "id": 47,
+    "type": "square",
+    "start": {
+      "col": 1,
+      "row": 2,
+      "dir": "N"
+    },
+    "end": {
+      "col": 2,
+      "row": 2
+    },
+    "pieces": {
+      "green": 3,
+      "yellow": 1,
+      "orange": 0
+    }
+  },
+  {
+    "id": 48,
+    "type": "square",
+    "start": {
+      "col": 2,
+      "row": 2,
+      "dir": "N"
+    },
+    "end": {
+      "col": 1,
+      "row": 2
+    },
+    "pieces": {
+      "green": 3,
+      "yellow": 1,
+      "orange": 0
+    }
+  },
+  {
+    "id": 49,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 3,
+      "row": 2
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 2,
+      "orange": 1
+    }
+  },
+  {
+    "id": 50,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 2,
+      "row": 3
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 2,
+      "orange": 1
+    }
+  },
+  {
+    "id": 51,
+    "type": "square",
+    "start": {
+      "col": 3,
+      "row": 3,
+      "dir": "NW"
+    },
+    "end": {
+      "col": 0,
+      "row": 2
+    },
+    "pieces": {
+      "green": 0,
+      "yellow": 4,
+      "orange": 0
+    }
+  },
+  {
+    "id": 52,
+    "type": "square",
+    "start": {
+      "col": 3,
+      "row": 0,
+      "dir": "SW"
+    },
+    "end": {
+      "col": 0,
+      "row": 2
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 2,
+      "orange": 1
+    }
+  },
+  {
+    "id": 53,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 1,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 2,
+      "row": 3
+    },
+    "pieces": {
+      "green": 2,
+      "yellow": 2,
+      "orange": 0
+    }
+  },
+  {
+    "id": 54,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 1,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 2,
+      "row": 3
+    },
+    "pieces": {
+      "green": 2,
+      "yellow": 2,
+      "orange": 0
+    }
+  },
+  {
+    "id": 55,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 0,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 3,
+      "row": 3
+    },
+    "pieces": {
+      "green": 2,
+      "yellow": 2,
+      "orange": 0
+    }
+  },
+  {
+    "id": 56,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 3,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 3,
+      "row": 0
+    },
+    "pieces": {
+      "green": 2,
+      "yellow": 3,
+      "orange": 1
+    }
+  },
+  {
+    "id": 57,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 2,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 3,
+      "row": 1
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 2
+    }
+  },
+  {
+    "id": 58,
+    "type": "square",
+    "start": {
+      "col": 3,
+      "row": 3,
+      "dir": "NW"
+    },
+    "end": {
+      "col": 0,
+      "row": 2
+    },
+    "pieces": {
+      "green": 2,
+      "yellow": 1,
+      "orange": 1
+    }
+  },
+  {
+    "id": 59,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 2,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 3,
+      "row": 1
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 2
+    }
+  },
+  {
+    "id": 60,
+    "type": "square",
+    "start": {
+      "col": 3,
+      "row": 2,
+      "dir": "NW"
+    },
+    "end": {
+      "col": 0,
+      "row": 1
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 2
+    }
+  },
+  {
+    "id": 61,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 1,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 3,
+      "row": 2
+    },
+    "pieces": {
+      "green": 2,
+      "yellow": 0,
+      "orange": 3
+    }
+  },
+  {
+    "id": 62,
+    "type": "square",
+    "start": {
+      "col": 0,
+      "row": 1,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 3,
+      "row": 2
+    },
+    "pieces": {
+      "green": 2,
+      "yellow": 0,
+      "orange": 3
+    }
+  },
+  {
+    "id": 63,
+    "type": "square",
+    "start": {
+      "col": 2,
+      "row": 2,
+      "dir": "NW"
+    },
+    "end": {
+      "col": 2,
+      "row": 1
+    },
+    "pieces": {
+      "green": 3,
+      "yellow": 0,
+      "orange": 2
+    }
+  },
+  {
+    "id": 64,
+    "type": "square",
+    "start": {
+      "col": 3,
+      "row": 1,
+      "dir": "SW"
+    },
+    "end": {
+      "col": 0,
+      "row": 2
+    },
+    "pieces": {
+      "green": 3,
+      "yellow": 0,
+      "orange": 2
+    }
+  },
+  {
+    "id": 65,
+    "type": "rect",
+    "start": {
+      "col": 1,
+      "row": 1,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 4,
+      "row": 2
+    },
+    "pieces": {
+      "green": 4,
+      "yellow": 2,
+      "orange": 1
+    }
+  },
+  {
+    "id": 66,
+    "type": "rect",
+    "start": {
+      "col": 1,
+      "row": 1,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 4,
+      "row": 2
+    },
+    "pieces": {
+      "green": 2,
+      "yellow": 2,
+      "orange": 3
+    }
+  },
+  {
+    "id": 67,
+    "type": "rect",
+    "start": {
+      "col": 1,
+      "row": 2,
+      "dir": "NE"
+    },
+    "end": {
+      "col": 4,
+      "row": 1
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 2,
+      "orange": 2
+    }
+  },
+  {
+    "id": 68,
+    "type": "rect",
+    "start": {
+      "col": 1,
+      "row": 2,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 4,
+      "row": 2
+    },
+    "pieces": {
+      "green": 2,
+      "yellow": 2,
+      "orange": 1
+    }
+  },
+  {
+    "id": 69,
+    "type": "rect",
+    "start": {
+      "col": 0,
+      "row": 2,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 4,
+      "row": 1
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 4,
+      "orange": 2
+    }
+  },
+  {
+    "id": 70,
+    "type": "rect",
+    "start": {
+      "col": 1,
+      "row": 1,
+      "dir": "SE"
+    },
+    "end": {
+      "col": 4,
+      "row": 2
+    },
+    "pieces": {
+      "green": 1,
+      "yellow": 1,
+      "orange": 4
+    }
+  }
+];
